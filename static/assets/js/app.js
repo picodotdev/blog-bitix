9bd79c85db749085c1c2f102c37b9e70452bbe55
pico.dev <pico.dev@gmail.com>
2018-03-07 20:20:44 +0100

    Pequeños retoques

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -46,8 +46,8 @@ require(['jquery'], function($) {
                 var html = [
                     '<div style="' + $(ad).attr('style') + '; height: 100%; text-align: left;" class="adblock">',
                     ' <p class="text-center"><strong><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Parece que tienes activado un bloqueador de anuncios</strong></p>',
-                    ' <p>Los anuncios de este blog <strong>no son intrusivos</strong> y con ellos hago <a href="https://picodotdev.github.io/blog-bitix/2015/12/yo-apoyo-al-software-libre-tu-tambien/">pequeñas donaciones al software libre</a>. Considera <a href="https://adblockplus.org/es/faq_basics#disable" target="_blank">desactivar el bloqueador de anuncios</a> en <strong>Blog Bitix</strong>.</p>',
-                    ' <p></p>',
+                    ' <p>Los anuncios de este blog <strong>no son intrusivos</strong> y con ellos hago <a href="https://picodotdev.github.io/blog-bitix/2015/12/yo-apoyo-al-software-libre-tu-tambien/">pequeñas donaciones al software libre</a>.</p>',
+                    ' <p>Si no es por privacidad considera <a href="https://adblockplus.org/es/faq_basics#disable" target="_blank">desactivar el bloqueador de anuncios</a> en <strong>Blog Bitix</strong>.</p>',
                     '</div>'
                 ].join('');
                 var dom = $(html);
