6412f1925f947f983c93b93f071d94a90e2d9de1
pico.dev <pico.dev@gmail.com>
2018-05-19 14:04:32 +0200

    Corregir imágenes en xml

--- build-deploy.sh
+++ build-deploy.sh
@@ -7,6 +7,7 @@ npm run less
 hugo --destination="deploy" --source="." --theme="bitix" --cleanDestinationDir
 # Renombrado assets en feeds
 find ./deploy -type f -name "index.xml" -exec sed -i '' 's/\&\#34;assets\//\&\#34;https:\/\/picodotdev.github.io\/blog-bitix\/assets\//g' {} +
+find ./deploy -type f -name "index.xml" -exec sed -i '' 's/\&\#34;https:\/\/picodotdev.github.io\/blog-bitix\/blog-bitix\//\&\#34;https:\/\/picodotdev.github.io\/blog-bitix\//g' {} +
 # Soporte para compatibilidad de feeds antiguos
 cp deploy/index.xml deploy/atom.xml
 mkdir -p deploy/categories/planeta-codigo/
