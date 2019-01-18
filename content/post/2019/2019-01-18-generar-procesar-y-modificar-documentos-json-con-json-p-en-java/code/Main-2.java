5badddba577e475f52d4d7b564ca1d81f0e1859a
pico.dev <pico.dev@gmail.com>
2019-01-18 19:33:19 +0100

    Pequeños retoques

--- content/post/2019/2019-01-18-generar-procesar-y-modificar-documentos-json-con-json-p-en-java/code/Main-2.java
+++ content/post/2019/2019-01-18-generar-procesar-y-modificar-documentos-json-con-json-p-en-java/code/Main-2.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ...
 
+        JsonPatch jsonPatch = Json.createPatchBuilder().add("/telefono", "111111111").remove("/direcciones/0").build();
         jsonp = Json.createPatchBuilder().add("/telefono", "111111111").remove("/direcciones/0").build().apply(jsonp);
-        System.out.printf("JSON-P (JsonPatch): %s%n", jsonp.toString());
+        System.out.printf("JSON-P (JsonPatch): %s%n", jsonPatch.toString());
+        System.out.printf("JSON-P (JsonObject): %s%n", jsonp.toString());
 
         ...
 }
