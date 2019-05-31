9239fd2b21921bbaa29b4856b4aeca6ea713fd01
pico.dev <pico.dev@gmail.com>
2019-05-31 20:37:24 +0200

    Pequeños retoques

--- content/post/2019/2019-05-31-implementar-la-paginacion-eficientemente-en-consultas-sql-con-seek/code/Seek.java
+++ content/post/2019/2019-05-31-implementar-la-paginacion-eficientemente-en-consultas-sql-con-seek/code/Seek.java
@@ -1,5 +1,9 @@
+import org.jooq.DSLContext;
+...
+private DSLContext context;
+...
 context.selectFrom(Tables.PRODUCTO)
-.orderBy(PRODUCTO.CANTIDAD, PRODUCTO.ID)
-.seek(3l, 2l)
-.limit(10)
-.fetchInto(Producto.class)
+    .orderBy(PRODUCTO.CANTIDAD, PRODUCTO.ID)
+    .seek(3l, 2l)
+    .limit(10)
+    .fetchInto(Producto.class);
