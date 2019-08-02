943541a7ad89fd9c9b90a9f1195efee30955d0e4
pico.dev <pico.dev@gmail.com>
2019-08-03 00:31:20 +0200

    Pequeños retoques

--- content/post/2019/2019-08-02-implementar-tolerancia-a-fallos-con-resilience4j/code/Resilience4jProxyService.java
+++ content/post/2019/2019-08-02-implementar-tolerancia-a-fallos-con-resilience4j/code/Resilience4jProxyService.java
@@ -35,10 +35,7 @@ public class Resilience4jProxyService {
         String resource = String.format("%s%s", uri.toString(), "/service");
         Invocation.Builder builder = ClientBuilder.newClient().target(resource).request();
 
-        Span span = tracer.newTrace().start();
-        TraceContext.Injector<Invocation.Builder> injector = tracing.propagation().injector((Invocation.Builder carrier, String key, String value) -> { carrier.header(key, value); });
-        injector.inject(span.context(), builder);
-        System.out.printf("Client Span (traceId: %s, spanId: %s)%n", span.context().traceIdString(), span.context().spanIdString());
+        ...
 
         CircuitBreaker circuitBreaker = CircuitBreaker.of("resilience4jCircuitBreakerProxyService", circuitBreakerConfiguration);
         TimeLimiter timeLimiter = TimeLimiter.of(timeLimiterConfiguration);
