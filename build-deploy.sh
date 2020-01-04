c5a42009ccda3e17fd5ac5c8a79870f40ca927e4
pico.dev <pico.dev@gmail.com>
2020-01-04 07:09:57 +0100

    Pequeños retoques

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,7 +3,9 @@ set -e
 
 (cd deploy/ && git pull)
 ./generate-styles.sh
-hugo --minify --destination="deploy" --source="." --cleanDestinationDir
+#https://github.com/gohugoio/hugo/issues/6699
+#hugo --minify --destination="deploy" --source="." --cleanDestinationDir
+hugo --destination="deploy" --source="." --cleanDestinationDir
 # Renombrado URLs absolutas en feeds
 find ./deploy -type f -name "index.xml" -exec sed -i 's|\"/blog-bitix/|\"https://picodotdev.github.io/blog-bitix/|g' {} +
 find ./deploy -type f -name "index.xml" -exec sed -i 's|\"https://picodotdev.github.io/blog-bitix/blog-bitix/|\"https://picodotdev.github.io/blog-bitix/|g' {} +
