d4055d8a9729fa60f1c8fd2dfec72b6387e65902
pico.dev <pico.dev@gmail.com>
2020-01-10 23:37:32 +0100

    Pequeños retoques

--- generate-styles.sh
+++ generate-styles.sh
@@ -8,4 +8,3 @@ hugo gen chromastyles --style=github > themes/bitix/static/assets/css/syntax.css
 
 cp themes/bitix/static/assets/css/syntax.css themes/bitix/layouts/partials/style-syntax.amp.css
 cp themes/bitix/static/assets/css/main.css themes/bitix/layouts/partials/style-main.amp.css
-cp themes/bitix/static/assets/css/main-cookieconsent.css themes/bitix/layouts/partials/style-main-cookieconsent.amp.css
