8d9c94e3671cb31b40a9b3be66edc8d70df13d04
pico.dev <pico.dev@gmail.com>
2020-11-21 15:00:22 +0100

    Actualización de librerías js/css

--- themes/bitix/static/assets/js/require-config.js
+++ themes/bitix/static/assets/js/require-config.js
@@ -1,16 +1,16 @@
 var require = {
     paths: {
-        'jquery': '../libs/jquery-3.5.0.min',
-        'bootstrap': '../libs/bootstrap-4.4.1-dist/js/bootstrap.min',
+        'jquery': '../libs/jquery-3.5.1.slim.min',
+        'bootstrap': '../libs/bootstrap-4.5.3-dist/js/bootstrap.min',
         'bowser': '../libs/bowser-2.8.1',
-        'lozad': '../libs/lozad-1.14.0.min',
-        'blueimp-helper': '../libs/Gallery-2.38.0/js/blueimp-helper',
-        'blueimp-gallery': '../libs/Gallery-2.38.0/js/blueimp-gallery',
-        'blueimp-gallery-indicator': '../libs/Gallery-2.38.0/js/blueimp-gallery-indicator',
-        'blueimp-gallery-video': '../libs/Gallery-2.38.0/js/blueimp-gallery-video',
-        'blueimp-gallery-vimeo': './libs/Gallery-2.38.0/js/blueimp-gallery-vimeo',
-        'blueimp-gallery-youtube': '../libs/Gallery-2.38.0/js/blueimp-gallery-youtube',
-        'jquery-blueimp-gallery': '../libs/Gallery-2.38.0/js/jquery.blueimp-gallery',
+        'lozad': '../libs/lozad-1.16.0.min',
+        'blueimp-helper': '../libs/Gallery-3.3.0/js/blueimp-helper',
+        'blueimp-gallery': '../libs/Gallery-3.3.0/js/blueimp-gallery',
+        'blueimp-gallery-indicator': '../libs/Gallery-3.3.0/js/blueimp-gallery-indicator',
+        'blueimp-gallery-video': '../libs/Gallery-3.3.0/js/blueimp-gallery-video',
+        'blueimp-gallery-vimeo': './libs/Gallery-3.3.0/js/blueimp-gallery-vimeo',
+        'blueimp-gallery-youtube': '../libs/Gallery-3.3.0/js/blueimp-gallery-youtube',
+        'jquery-blueimp-gallery': '../libs/Gallery-3.3.0/js/jquery.blueimp-gallery',
         'cookieconsent': 'https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min'
     },
     shim: {
