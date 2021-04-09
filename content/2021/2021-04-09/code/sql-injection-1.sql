f40d06f0767aea43a16294bda888257a2e2d4dfb
pico.dev <pico.dev@gmail.com>
2021-04-09 20:35:13 +0200

    Pequeños retoques

--- content/2021/2021-04-09/code/sql-injection-1.sql
+++ content/2021/2021-04-09/code/sql-injection-1.sql
@@ -1,3 +1,3 @@
-"SELECT id, " + column + " FROM users WHERE user_id = " + userId;
+"SELECT id, " + column + " FROM users WHERE user_id = " + userId
 
-"SELECT id, password FROM users WHERE user_id = " + userId;
+"SELECT id, password FROM users WHERE user_id = 1
